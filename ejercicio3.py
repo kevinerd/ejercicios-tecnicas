de098716d051ab65679fa58aaf76330bd611bdb3
Kevin <kevinjf2011@gmail.com>
2017-03-23 14:40:27 -0300

    Agrego ejercicio3.

---
+++ ejercicio3.py
@@ -0,0 +1,9 @@
+valor_compra = input("Ingrese el total de la compra: $")
+valor_compra = int(valor_compra)
+
+tipo_compra = input("¿Qué método de pago utilizará? contado/tarjeta")
+
+if(tipo_compra = contado):
+  print("La operación tiene un descuento del 10%. El nuevo valor total es: $" + ((valor_compra * 8) / 100) )
+else:
+    print("La operación no tiene descuento.")
